b83a6a663860cc369583aad16fb2be5d41449f99
Federico-Panigari <federico.panigari@edu-its.it>
2026-09-14 10:53:20 +0000

    todolist-implemented-DA-RIGUARDARE

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './style.css'
 import { somma } from './matematica.ts'
 import type { user } from './user.ts'
+import type { Todo } from './todo.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<h1>calcolatrice</h1>
 
-`
+
+//calcolatrice e user
 somma(5,3)
 
 const utente: user= {
@@ -13,4 +13,47 @@ const utente: user= {
   eta: 19
 }
 
-console.log(utente.eta)
+
+//Todo
+let tasks : Todo[]= []
+
+const input = document.querySelector<HTMLInputElement>("#nuovaTask")!
+const bottone = document.querySelector<HTMLButtonElement>("#aggiungi")!
+const lista = document.querySelector<HTMLUListElement>("#lista")!
+
+// Evento click
+bottone.addEventListener("click", () => {
+
+  const testo = input.value
+
+  const nuovaTodo: Todo = {
+    testo: testo
+  }
+
+  tasks.push(nuovaTodo)
+
+  lista.innerHTML += `<li>${nuovaTodo.testo}</li>`
+
+  input.value = ""
+})
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<h1>calcolatrice</h1>
+
+ <br><br/>
+
+<h1>To-Do-List</h1>
+
+    <form>
+      <label for="nuovaTask">nuova task</label>
+      <input type="string" id="nuovaTask" name="nuovaTask"> 
+    </form> 
+    </br>
+    <button id="bottone">Aggiungi</button> 
+
+    <ul id="${lista}">
+    
+    </ul>
+
+`
+
---
+++ src/todo.ts
@@ -0,0 +1,5 @@
+export interface Todo {
+    id : number,
+    descrizione: string,
+    completata: boolean
+}
